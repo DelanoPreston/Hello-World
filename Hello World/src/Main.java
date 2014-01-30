753d30056b4cb2ee036d17924acddf645b3f6ac0
Preston Delano <Delanopreston@gmail.com>
2014-01-29 23:13:39 -0700

    first Change

--- Hello World/src/Main.java
+++ Hello World/src/Main.java
@@ -5,6 +5,7 @@ public class Main {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
 		System.out.println("Version 1");
+		System.out.println("this is my change!!!");
 	}
 
 }
